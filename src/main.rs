b14d060617968f0911b190acb6b51ea4919d41d7
llamicron <llamicron@gmail.com>
2019-06-15 23:33:36 -0500

    Adds some more Server impl

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
+
 mod server;
+
+use std::process::Command;
+use server::Server;
+use std::str;
+
+fn main() {
+    let server = Server::new("Brewpi", "llamicron", "192.168.0.72", "right hur dude").expect("Something went wrong :(");
+    server.connect();
+
+    // let mut ssh = Command::new("sh");
+    // ssh.arg("-c").arg(format!("{} {}", "ssh", server.address()));
+    // // ssh.arg(server.address());
+
+    // let output = ssh.output().expect("Something went wrong :(");
+    // println!("{:?}", output);
+}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 use std::net::Ipv4Addr;
 use std::fmt;
+use std::process::{Command, ExitStatus};
 
 pub struct Server {
     name: String,
@@ -9,7 +10,7 @@ pub struct Server {
 }
 
 impl Server {
-    fn new(name: &str, username: &str, ip: &str, location: &str) -> Result<Server, &'static str> {
+    pub fn new(name: &str, username: &str, ip: &str, location: &str) -> Result<Server, &'static str> {
         let parsed_ip = match ip.parse::<Ipv4Addr>() {
             Ok(x) => x,
             Err(_) => return Err("Invalid IP Address")
@@ -23,7 +24,21 @@ impl Server {
         };
         Ok(server)
     }
+
+    pub fn address(&self) -> String {
+        format!("{}@{}", self.username, self.ip)
+    }
+
+    pub fn connect(&self) -> ExitStatus {
+        let mut ssh = Command::new("sh");
+        ssh.arg("-c");
+        ssh.arg(format!("ssh {}", self.address()));
+
+        return ssh.status().expect("Something went horribly wrong :(");
+    }
 }
+
+
 
 impl fmt::Display for Server {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -31,13 +46,23 @@ impl fmt::Display for Server {
     }
 }
 
+
+
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn tester_server() -> Server {
+        Server::new("Brewpi", "llamicron", "192.168.0.1", "Outside").expect("Something went wrong :(")
+    }
+
     #[test]
     fn new_server() {
-        let server = Server::new("Brewpi", "llamicron", "192.168.0.1", "Outside").expect("Something went wrong :(");
+        let server = tester_server();
         assert_eq!(server.ip, Ipv4Addr::new(192, 168, 0, 1));
         assert_eq!(server.name,     String::from("Brewpi"));
         assert_eq!(server.username, String::from("llamicron"));
@@ -45,8 +70,14 @@ mod tests {
     }
 
     #[test]
+    fn address() {
+        let server = tester_server();
+        assert_eq!(server.address(), String::from("llamicron@192.168.0.1"));
+    }
+
+    #[test]
     fn display() {
-        let server = Server::new("Brewpi", "llamicron", "192.168.0.1", "Outside").expect("Something went wrong :(");
+        let server = tester_server();
         assert_eq!(format!("{}", server), "Brewpi: llamicron@192.168.0.1; Outside");
     }
 }
