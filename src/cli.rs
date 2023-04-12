2f473e2f26229f84a44a3536eea61cf72fac6a81
llamicron <llamicron@gmail.com>
2023-04-11 19:23:35 -0500

    Adds required input method for cli

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,13 @@
 
 use std::io::{BufRead, self};
 
+#[derive(Debug, thiserror::Error)]
+pub enum CLIError {
+    #[error("Required value")]
+    NoValue,
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error)
+}
 
 pub struct CLI<R: BufRead> {
     reader: R
@@ -13,10 +20,24 @@ impl<R: BufRead> CLI<R> {
         CLI { reader }
     }    
 
-    pub fn read_line(&mut self) -> Result<String, io::Error> {
+    /// Just reads a line from the buffer
+    pub fn read_line(&mut self) -> Result<String, CLIError> {
         let mut buffer = String::new();
         self.reader.read_line(&mut buffer)?;
         Ok(buffer)
+    }
+
+    /// Will return an Ok(String) with some content (non empty) or an error
+    pub fn required_input(&mut self, prompt: &str) -> Result<String, CLIError> {
+        print!("{}", prompt);
+        io::Write::flush(&mut io::stdout())?;
+
+        let input = self.read_line()?;
+        if input.trim().is_empty() {
+            Err(CLIError::NoValue)
+        } else {
+            Ok(input)
+        }
     }
 
 }
@@ -48,8 +69,8 @@ mod tests {
     #[test]
     fn test_required_input() {
         // idk if this is how I want it to work
-        // assert!(cli("").required_input().is_err());
-        // assert_eq!(cli("something").required_input(), Ok("something"));
-        // assert!(cli("   ").required_input().is_err());
+        assert!(cli("").required_input("").is_err());
+        assert_eq!(cli("something").required_input("").unwrap(), "something".to_string());
+        assert!(cli("   ").required_input("").is_err());
     }
 }
